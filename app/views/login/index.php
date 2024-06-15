d0868c7cf24a93c7c68b65b73355f8c49e173120
Oluwasesan Adeleke <oadeleke@algomau.ca>
2024-06-15 04:00:18 +0000

    removed debugging

--- app/views/login/index.php
+++ app/views/login/index.php
@@ -3,13 +3,8 @@
 <main role="main" class="container">
 	<div class="login-container">
 			<h1>Login</h1>
-		<?php
-		// Debugging
-		echo "<!--\n";
-		echo "error: " . (isset($error) ? $error : "not set") . "\n";
-		echo "success: " . (isset($success) ? $success : "not set") . "\n";
-		echo "-->\n";
-		?>
+			<?php if (!empty($error)) echo "<p class='error-message'>$error</p>"; ?>
+			<?php if (!empty($success)) echo "<p class='success-message'>$success</p>"; ?>
 			<form action="/login/verify" method="post">
 					<input type="text" name="username" placeholder="Username" required>
 					<input type="password" name="password" placeholder="Password" required>
