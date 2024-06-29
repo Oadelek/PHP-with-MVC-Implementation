0c8053ebfeffc8dbf8f46b57e318f15a82e816d2
Oluwasesan Adeleke <oadeleke@algomau.ca>
2024-06-29 20:18:13 +0000

    Added file for styling

--- app/views/reminder/index.php
+++ app/views/reminder/index.php
@@ -4,7 +4,8 @@
     <h1>My Reminders</h1>
     <a href="/reminder/create" class="btn btn-primary">Create New Reminder</a>
     <ul class="list-group mt-3">
-        <?php foreach ($notes as $note): ?>
+        <?php 
+        foreach ($notes as $note): ?>
             <li class="list-group-item d-flex justify-content-between align-items-center">
                 <?php echo htmlspecialchars($note['subject']); ?>
                 <span>
